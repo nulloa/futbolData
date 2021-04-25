89a58407a7447063f1f7d49e2c3fb0004f04239f
Nehemias Ulloa <nulloa1@iastate.edu>
2021-04-25 16:02:11 -0500

    Fix Season character

--- data-raw/cleanData.R
+++ data-raw/cleanData.R
@@ -2,7 +2,9 @@
 tmp <- read.csv("laliga.csv")
 
 # Remove columns associated with betting
-tmp <- data.frame(Season=tmp$Season, tmp[c(2:22)])
+tmp <- data.frame(fSeason=tmp$Season, tmp[c(2:22)])
+tmp$Season  = paste0("20", substr(tmp$fSeason, start = 1, stop = 2))
+
 
 # Create differences: Home - Away
 laliga = data.frame(tmp[1:4],
