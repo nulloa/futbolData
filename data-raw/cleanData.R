341e6a481392262f5598657a3bd3bdab3d962d16
Nehemias Ulloa <nulloa1@iastate.edu>
2021-04-25 16:30:50 -0500

    Account for new season var

--- data-raw/cleanData.R
+++ data-raw/cleanData.R
@@ -1,10 +1,13 @@
+# Getwd()
+here::here()
+
 # Read in raw data
 tmp <- read.csv("laliga.csv")
 
 # Remove columns associated with betting
-tmp <- data.frame(fSeason=tmp$Season, tmp[c(2:22)])
+tmp <- data.frame(fSeason=tmp$fSeason, tmp[c(2:23)])
+tmp$fSeason[nchar(tmp$fSeason)==3] <- paste0("0", tmp$fSeason[nchar(tmp$fSeason)==3])
 tmp$Season  = paste0("20", substr(tmp$fSeason, start = 1, stop = 2))
-
 
 # Create differences: Home - Away
 laliga = data.frame(tmp["Season"],
