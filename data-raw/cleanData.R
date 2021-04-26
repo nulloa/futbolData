08807a8445c6a13ebe3c8073ecb5ea36df7b5dd0
Nehemias Ulloa <nulloa1@iastate.edu>
2021-04-25 21:10:36 -0500

    Add missing away team variable.

--- data-raw/cleanData.R
+++ data-raw/cleanData.R
@@ -5,13 +5,13 @@ here::here()
 tmp <- read.csv("laliga.csv")
 
 # Remove columns associated with betting
-tmp <- data.frame(fSeason=tmp$fSeason, tmp[c(2:23)])
+tmp <- data.frame(fSeason=tmp$Season, tmp[c(2:23)])
 tmp$fSeason[nchar(tmp$fSeason)==3] <- paste0("0", tmp$fSeason[nchar(tmp$fSeason)==3])
 tmp$Season  = paste0("20", substr(tmp$fSeason, start = 1, stop = 2))
 
 # Create differences: Home - Away
 laliga = data.frame(tmp["Season"],
-                    tmp[1:4],
+                    tmp[1:5],
                     FinalScore = tmp$FTHG - tmp$FTAG,
                     FinalRes   = tmp$FTR,
                     HalfScore  = tmp$HTHG - tmp$HTAG,
