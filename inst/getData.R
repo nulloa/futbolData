b0e25b1541e399333309e9f4b001fabe40251b66
Nehemias Ulloa <nulloa1@iastate.edu>
2023-08-27 22:27:41 -0700

    Add 3 most recent seasons to the dataset

--- inst/getData.R
+++ inst/getData.R
@@ -2,21 +2,24 @@ library(readr)
 library(dplyr)
 
 # Setup locations of csv files
-files = c("http://www.football-data.co.uk/mmz4281/1920/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1819/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1718/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1617/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1516/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1415/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1314/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1213/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1112/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/1011/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/0910/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/0809/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/0708/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/0607/SP1.csv",
-          "http://www.football-data.co.uk/mmz4281/0506/SP1.csv")
+files = c("https://www.football-data.co.uk/mmz4281/2223/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/2122/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/2021/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1920/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1819/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1718/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1617/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1516/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1415/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1314/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1213/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1112/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/1011/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/0910/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/0809/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/0708/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/0607/SP1.csv",
+          "https://www.football-data.co.uk/mmz4281/0506/SP1.csv")
 
 # Create function to read csv and add season
 read_football_csv = function(x){
@@ -28,4 +31,4 @@ read_football_csv = function(x){
 # Merge into one data set
 laliga = lapply(files, read_football_csv) %>% bind_rows()
 
-write.csv(laliga, file="laliga.csv", row.names=FALSE)
+write.csv(laliga, file="extdata/laliga.csv", row.names=FALSE)
